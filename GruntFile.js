6c6e6703550fe0ca72dce7d462d0d3015a70c58c
ajay2507 <pajay2507@gmail.com>
2018-04-01 18:19:17 -0700

    loaded default tasks

--- GruntFile.js
+++ GruntFile.js
@@ -1,4 +1,23 @@
 module.exports = (grunt) => {
     'use strict';
-    
-}
+    grunt.initConfig({
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'tasks/*.js'
+            ],
+            options: {
+                jshintrc: '.jshintrc',
+            }
+        },
+        lasso: {
+
+        }
+
+    });
+    console.log(grunt);
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    //grunt.loadNpmTasks('grunt-contrib-clean');
+    // loading default task
+    grunt.registerTask('default', ['jshint']);
+};
